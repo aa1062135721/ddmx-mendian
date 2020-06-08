8acb958baf5df90845b5aeb475e4ef4becb8101f
李琦 <1062135721@qq.com>
2020-06-08 12:56:28 +0800

    添加密码登录功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,29 +7,36 @@
                     <br>
                     <p>门店管理系统</p>
                 </div>
-
-                <el-input  class="urser-name"  placeholder="请输入用户手机号码" v-model="username" @keyup.enter.native="submitForm" prefix-icon="el-icon-user" maxlength="11">
-                </el-input>
-
-                <el-input class="password"  placeholder="请输入验证码" v-model="password" @keyup.enter.native="submitForm" prefix-icon="el-icon-lock" maxlength="6">
-                    <el-button slot="suffix" round plain size="small" @click="getCode" v-if="canClick" class="sed-code">{{content}}</el-button>
-                    <el-button slot="suffix" round plain size="small" v-else class="re-sed-code">{{content}}</el-button>
-                </el-input>
-
-                <div class="tips" v-if="msg">
+                <div v-if="type===1">
+                  <el-input class="userNumber" v-model="userNumber" placeholder="请输入手机号码" clearable  @keyup.enter.native="submitForm"  prefix-icon="el-icon-user"  maxlength="11"></el-input>
+                  <el-input class="userPwd" v-model="userPwd" placeholder="请输入密码"  show-password  @keyup.enter.native="submitForm" prefix-icon="el-icon-lock" maxlength="6"></el-input>
+                  <div class="tips" v-if="msg">
                     <p>{{msg}}</p>
+                  </div>
+                  <div class="login-btn">
+                    <el-button type="primary" @click="Loginbtn()">登录</el-button>
+                  </div>
+               </div>
+                <div v-else>
+                  <el-input  class="urser-name"  placeholder="请输入手机号码" clearable v-model="username" @keyup.enter.native="submitForm" prefix-icon="el-icon-user" maxlength="11">
+                  </el-input>
+                  <el-input class="password"  placeholder="请输入验证码" v-model="password" @keyup.enter.native="submitForm" prefix-icon="el-icon-lock" maxlength="6">
+                      <el-button slot="suffix" round plain size="small" @click="getCode" v-if="canClick" class="sed-code">{{content}}</el-button>
+                      <el-button slot="suffix" round plain size="small" v-else class="re-sed-code">{{content}}</el-button>
+                  </el-input>
+                  <div class="tips" v-if="msg">
+                      <p>{{msg}}</p>
+                  </div>
+                  <div class="login-btn">
+                      <el-button type="primary" @click="submitForm()">登录</el-button>
+                  </div>
                 </div>
-
-                <div class="login-btn">
-                    <el-button type="primary" @click="submitForm()">登录</el-button>
-                </div>
-
-<!--              <div class="login-type" v-show="type === 2" @click="type = 1">-->
-<!--                <p>手机验证码登录</p>-->
-<!--              </div>-->
-<!--              <div class="login-type" v-show="type === 1" @click="type = 2">-->
-<!--                <p>微信二维码登录</p>-->
-<!--              </div>-->
+             <div class="login-type" v-if="type===1" @click="type=2">
+               <p>输入验证码登录></p>
+             </div>
+             <div class="login-type" v-else @click="verification">
+               <p>输入密码登录></p>
+             </div>
             </el-form>
         </div>
     </div>
@@ -37,24 +44,35 @@
 
 <script>
 import { mapMutations } from 'vuex'
-import {postUserInfo, postLoginGetCode, postLoginByCode} from '../api/getData'
+import {postUserInfo, postLoginGetCode, postLoginByCode, postLoginGetPwd} from '../api/getData'
 import {setCookie} from '../utils'
+// eslint-disable-next-line no-unused-vars
+import { async } from 'q'
+// eslint-disable-next-line no-unused-vars
+import qs from 'qs'
 export default {
-  data: function () {
+  data () {
     return {
       msg: '',
       username: '',
       password: '',
-      type:1,//1短信验证码登录，2微信二维码登录
-      //发送验证码按钮所需要的数据
+      userNumber: '',
+      userPwd: '',
+      type: 1, // 1短信验证码登录，2微信二维码登录
+      // 发送验证码按钮所需要的数据
       canClick: true,
-      content: '发送验证码',  // 按钮里显示的内容
-      totalTime: 60,      //记录具体倒计时时间
+      content: '发送验证码', // 按钮里显示的内容
+      totalTime: 60 //  记录具体倒计时时间
     }
   },
   methods: {
-    //获取验证码
-    async getCode(){
+    // 点击切换密码登录
+    verification () {
+      this.type = 1
+      this.msg = ''
+    },
+    // 获取验证码
+    async getCode () {
       if (!/^[1][3,4,5,7,8,9][0-9]{9}$/.test(this.username)) {
         this.msg = '用户名应为手机号码'
         return
@@ -62,7 +80,6 @@ export default {
       if (!this.canClick) {
         return
       }
-
       let data = {mobile: this.username}
       await postLoginGetCode(data).then(res => {
         if (res.code === 200) {
@@ -76,17 +93,39 @@ export default {
           this.content = `重新发送(${this.totalTime})`
           let clock = setInterval(() => {
             this.totalTime--
-            this.content =  `重新发送(${this.totalTime})`
+            this.content = `重新发送(${this.totalTime})`
             if (this.totalTime < 0) {
               clearInterval(clock)
               this.content = '发送验证码'
               this.totalTime = 60
-              this.canClick = true  //这里重新开启
+              this.canClick = true // 这里重新开启
             }
-          },1000)
+          }, 1000)
         }
       })
     },
+    // 密码登录
+    async Loginbtn () {
+      // eslint-disable-next-line no-undef
+      let data = {username: this.userNumber, password: this.userPwd}
+      let that = this
+      await postLoginGetPwd(data).then(async (res) => {
+        if (res.code === '200') {
+          await setCookie('token', res.data.token) // 浏览器保存登录凭证 这儿没有设置cookie的过期时间，默认是关闭浏览器就过期
+          await that.setToken(res.data.token) // vuex保存登录凭证
+          await postUserInfo().then(async (res) => {
+            await that.setUserInfo(res.data) // vuex保存登录后的登录数据
+            await that.$router.push({
+              path: '/money',
+              query: {}
+            })
+          })
+        } else {
+
+        }
+      })
+    },
+    // 验证码登录
     async submitForm () {
       if (this.username === '' || this.password === '') {
         this.msg = '请输入用户名或验证码'
@@ -100,7 +139,7 @@ export default {
       let that = this
       await postLoginByCode(data).then(async (res) => {
         if (res.code === '200') {
-          await setCookie('token', res.data.token) //浏览器保存登录凭证 这儿没有设置cookie的过期时间，默认是关闭浏览器就过期
+          await setCookie('token', res.data.token) // 浏览器保存登录凭证 这儿没有设置cookie的过期时间，默认是关闭浏览器就过期
           await that.setToken(res.data.token) // vuex保存登录凭证
           await postUserInfo().then(async (res) => {
             await that.setUserInfo(res.data) // vuex保存登录后的登录数据
@@ -137,6 +176,17 @@ export default {
         border-radius: 5px;
         background: #fff;
         text-align: center;
+        .userNumber,.userPwd{
+          width: 324px;
+          margin-bottom: 20px;
+          text-align: center;
+          /deep/ .el-input__inner{
+            height:48px;
+            background:rgba(255,255,255,1);
+            border-radius:8px;
+            font-size: 16px!important;
+          }
+        }
         .urser-name {
           width:324px;
           text-align: center;
@@ -201,13 +251,13 @@ export default {
         }
         .login-type{
           width:100%;
-          margin-top: 5px;
+          margin-top: 20px;
           p{
             width:324px;
             /* border: 1px solid black; */
             margin: auto;
             text-align: right;
-            color: #808080;
+            color: #161616;
             font-size: 16px;
           }
         }
